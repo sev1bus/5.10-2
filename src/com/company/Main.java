3fef9769089451010b34cbe9951d5bded62a4b5e
sev1bus <101794447+sev1bus@users.noreply.github.com>
2022-03-27 23:33:51 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 2;
+        for (int i=0; i<5000 ; i=i+2*a-1) {
+            System.out.println(i);
+        }
+
+
+
+    }
+}
